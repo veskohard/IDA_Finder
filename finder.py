9882972b5e959b53896659f1a6ad008d1c14216d
veskohard <vesko_hard@abv.bg>
2025-02-28 17:16:39 +0200

    Add files via upload
    IDA 9.x support

--- finder.py
+++ finder.py
@@ -19,65 +19,95 @@ class myplugin_t(idaapi.plugin_t):
         return idaapi.PLUGIN_OK
 
     def run(self, arg):
-        if idaapi.cvar.inf.is_be():
-            print("is_be = true")
-            #ea = idc.MinEA()
-            ea = ida_ida.inf_get_min_ea()
-            aaa = idc.get_screen_ea()
-            rett = []
+        is_be = ida_ida.inf_is_be()
+        rett = []
+        aaa = idc.get_screen_ea()
+        s=''
+        pattern = idaapi.compiled_binpat_vec_t()
+
+        if is_be:
+            s='BE'
+            print("ENDIAN: %s" % s)
             a4 = aaa >> 24 & 0xFF
             a3 = aaa >> 16 & 0xFF
             a2 = aaa >> 8 & 0xFF
             a1 = aaa & 0xFF
             bin_str = '%0.2X %0.2X %0.2X %0.2X' % (a4, a3, a2, a1)
-            while True:
-                ea = ida_search.find_binary(ea, idaapi.BADADDR, bin_str, 16, 1 | 4,)
-                if ea == idaapi.BADADDR:
-                    break
-                rett.append(ea)
-                print("Found BE offset -> 0x%0.8X" % ea)
-                ea += 1
-            if not rett:
-                print("BE offset not found [%s]" % bin_str)
+            idaapi.parse_binpat_str(pattern,ida_ida.inf_get_min_ea(),bin_str,16,0)
             
         else:
-            print("is_be = false")
-            ea = ida_ida.inf_get_min_ea()
-            aaa = idc.get_screen_ea()
-            ret = []
+            s='LE'
+            print("ENDIAN: %s" % s)
             a4 = aaa >> 24 & 0xFF
             a3 = aaa >> 16 & 0xFF
             a2 = aaa >> 8 & 0xFF
             a1 = aaa & 0xFF
             bin_str = '%0.2X %0.2X %0.2X %0.2X' % (a1, a2, a3, a4)
-            while True:
-                ea = ida_search.find_binary(ea, idaapi.BADADDR, bin_str, 16, 1 | 4,)
-                if ea == idaapi.BADADDR:
-                    break
-                ret.append(ea)
-                print("Found LE offset -> 0x%0.8X" % ea)
-                ea += 1
-            if not ret:
-                print("LE offset not match [%s]" % bin_str)
-
-            ea = ida_ida.inf_get_min_ea()
-            aaa = idc.get_screen_ea()
-            rett = []
+            idaapi.parse_binpat_str(pattern,ida_ida.inf_get_min_ea(),bin_str,16,0)
+        #adr = 0
+        adr = idaapi.inf_get_min_ea()
+        pat_len = sum(len(pat.bytes) for pat in pattern)
+        match=0
+        idaapi.msg("Searching for: %s, endian: %s, pattern length: %d\n" % (bin_str,s,pat_len))
+        while True:
+            adr = idaapi.bin_search(adr,ida_ida.inf_get_max_ea(),pattern,idaapi.BIN_SEARCH_CASE)
+            if adr == idaapi.BADADDR:
+                break
+            if int(adr[0]) > ida_ida.inf_get_max_ea()-pat_len:
+                break
+            print("Found offset %08X" % (int(adr[0])))
+            match += 1
+            adr = adr[0] + 1
+            
+        if match == 0:
+            print("data not found [%s]" % bin_str)  
+        else: 
+            print("data not found %d times" % match)  
+          
+        
+        procesor = idc.get_processor_name()
+        if procesor.startswith("ARM"):
+            print("ARM")  
             aaa = aaa + 1
-            a4 = aaa >> 24 & 0xFF
-            a3 = aaa >> 16 & 0xFF
-            a2 = aaa >> 8 & 0xFF
-            a1 = aaa & 0xFF
-            bin_str = '%0.2X %0.2X %0.2X %0.2X' % (a1, a2, a3, a4)
+            if is_be:
+                s='BE'
+                print("ENDIAN: %s" % s)
+                a4 = aaa >> 24 & 0xFF
+                a3 = aaa >> 16 & 0xFF
+                a2 = aaa >> 8 & 0xFF
+                a1 = aaa & 0xFF
+                bin_str = '%0.2X %0.2X %0.2X %0.2X' % (a4, a3, a2, a1)
+                idaapi.parse_binpat_str(pattern,ida_ida.inf_get_min_ea(),bin_str,16,0)
+            
+            else:
+                s='LE'
+                print("ENDIAN: %s" % s)
+                a4 = aaa >> 24 & 0xFF
+                a3 = aaa >> 16 & 0xFF
+                a2 = aaa >> 8 & 0xFF
+                a1 = aaa & 0xFF
+                bin_str = '%0.2X %0.2X %0.2X %0.2X' % (a1, a2, a3, a4)
+                idaapi.parse_binpat_str(pattern,ida_ida.inf_get_min_ea(),bin_str,16,0)
+        
+            adr = idaapi.inf_get_min_ea()
+            pat_len = sum(len(pat.bytes) for pat in pattern)
+            match=0
+            idaapi.msg("Searching for: %s, endian: %s, pattern length: %d\n" % (bin_str,s,pat_len))
             while True:
-                ea = ida_search.find_binary(ea, idaapi.BADADDR, bin_str, 16, 1 | 4,)
-                if ea == idaapi.BADADDR:
+                adr = idaapi.bin_search(adr,ida_ida.inf_get_max_ea(),pattern,idaapi.BIN_SEARCH_CASE)
+                if adr == idaapi.BADADDR:
                     break
-                rett.append(ea)
-                print("Found ARM call -> 0x%0.8X" % ea)
-                ea += 1
-            if not rett:
-                print("ARM call not match [%s]" % bin_str)
+                if int(adr[0]) > ida_ida.inf_get_max_ea()-pat_len:
+                    break
+                print("Found ARM call %08X" % (int(adr[0])))
+                match += 1
+                adr = adr[0] + 1
+                
+            if match == 0:
+                print("data not found [%s]" % bin_str)  
+            else: 
+                print("data not found %d times" % match)  
+            
 
     def term(self):
         pass
