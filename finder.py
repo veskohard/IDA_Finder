0fd58fa2cfe65ae5990b8cc4f24839def65a105f
veskohard <vesko_hard@abv.bg>
2025-03-03 17:27:34 +0200

    Improoved IDA 9 support

--- finder.py
+++ finder.py
@@ -5,113 +5,65 @@ import idaapi
 import idc
 import ida_search
 import ida_ida
+import ida_bytes
 import ida_idaapi
 
 class myplugin_t(idaapi.plugin_t):
-
     flags = idaapi.PLUGIN_UNL
     comment = 'IDA Offset/Reference Finder by vesko_hard'
     help = 'This is help'
-    wanted_name = 'IDA_Finder plugin'
+    wanted_name = 'IDA_Finder plugin '
     wanted_hotkey = 'F10'
 
     def init(self):
         return idaapi.PLUGIN_OK
 
+    def _generate_pattern(self, addr, is_be):
+        a4, a3, a2, a1 = (addr >> 24 & 0xFF, addr >> 16 & 0xFF, addr >> 8 & 0xFF, addr & 0xFF)
+        if is_be:
+            bin_str = f"{a4:02X} {a3:02X} {a2:02X} {a1:02X}"
+        else:
+            bin_str = f"{a1:02X} {a2:02X} {a3:02X} {a4:02X}"
+        return bin_str
+
+    def _search_pattern(self, bin_str, is_be):
+        idaapi.msg(f"Searching for: {bin_str}, endian: {'BE' if is_be else 'LE'}\n")
+        adr = ida_ida.inf_get_min_ea()
+        match_count = 0
+
+        while True:
+            adr = ida_bytes.find_bytes(bin_str, adr, radix=16)
+            if adr == ida_idaapi.BADADDR:
+                break
+
+            idaapi.msg(f"Found offset {adr:08X}\n")
+            match_count += 1
+            adr += 1  # Move to the next search position
+
+        if match_count == 0:
+            idaapi.msg(f"Data not found [{bin_str}]\n")
+        else:
+            idaapi.msg(f"Data found {match_count} times.\n")
+
     def run(self, arg):
         is_be = ida_ida.inf_is_be()
-        rett = []
-        aaa = idc.get_screen_ea()
-        s=''
-        pattern = idaapi.compiled_binpat_vec_t()
+        addr = idc.get_screen_ea()
 
-        if is_be:
-            s='BE'
-            print("ENDIAN: %s" % s)
-            a4 = aaa >> 24 & 0xFF
-            a3 = aaa >> 16 & 0xFF
-            a2 = aaa >> 8 & 0xFF
-            a1 = aaa & 0xFF
-            bin_str = '%0.2X %0.2X %0.2X %0.2X' % (a4, a3, a2, a1)
-            idaapi.parse_binpat_str(pattern,ida_ida.inf_get_min_ea(),bin_str,16,0)
-            
-        else:
-            s='LE'
-            print("ENDIAN: %s" % s)
-            a4 = aaa >> 24 & 0xFF
-            a3 = aaa >> 16 & 0xFF
-            a2 = aaa >> 8 & 0xFF
-            a1 = aaa & 0xFF
-            bin_str = '%0.2X %0.2X %0.2X %0.2X' % (a1, a2, a3, a4)
-            idaapi.parse_binpat_str(pattern,ida_ida.inf_get_min_ea(),bin_str,16,0)
-        #adr = 0
-        adr = idaapi.inf_get_min_ea()
-        pat_len = sum(len(pat.bytes) for pat in pattern)
-        match=0
-        idaapi.msg("Searching for: %s, endian: %s, pattern length: %d\n" % (bin_str,s,pat_len))
-        while True:
-            adr = idaapi.bin_search(adr,ida_ida.inf_get_max_ea(),pattern,idaapi.BIN_SEARCH_CASE)
-            if adr == idaapi.BADADDR:
-                break
-            if int(adr[0]) > ida_ida.inf_get_max_ea()-pat_len:
-                break
-            print("Found offset %08X" % (int(adr[0])))
-            match += 1
-            adr = adr[0] + 1
-            
-        if match == 0:
-            print("data not found [%s]" % bin_str)  
-        else: 
-            print("data not found %d times" % match)  
-          
-        
-        procesor = idc.get_processor_name()
-        if procesor.startswith("ARM"):
-            print("ARM")  
-            aaa = aaa + 1
-            if is_be:
-                s='BE'
-                print("ENDIAN: %s" % s)
-                a4 = aaa >> 24 & 0xFF
-                a3 = aaa >> 16 & 0xFF
-                a2 = aaa >> 8 & 0xFF
-                a1 = aaa & 0xFF
-                bin_str = '%0.2X %0.2X %0.2X %0.2X' % (a4, a3, a2, a1)
-                idaapi.parse_binpat_str(pattern,ida_ida.inf_get_min_ea(),bin_str,16,0)
-            
-            else:
-                s='LE'
-                print("ENDIAN: %s" % s)
-                a4 = aaa >> 24 & 0xFF
-                a3 = aaa >> 16 & 0xFF
-                a2 = aaa >> 8 & 0xFF
-                a1 = aaa & 0xFF
-                bin_str = '%0.2X %0.2X %0.2X %0.2X' % (a1, a2, a3, a4)
-                idaapi.parse_binpat_str(pattern,ida_ida.inf_get_min_ea(),bin_str,16,0)
-        
-            adr = idaapi.inf_get_min_ea()
-            pat_len = sum(len(pat.bytes) for pat in pattern)
-            match=0
-            idaapi.msg("Searching for: %s, endian: %s, pattern length: %d\n" % (bin_str,s,pat_len))
-            while True:
-                adr = idaapi.bin_search(adr,ida_ida.inf_get_max_ea(),pattern,idaapi.BIN_SEARCH_CASE)
-                if adr == idaapi.BADADDR:
-                    break
-                if int(adr[0]) > ida_ida.inf_get_max_ea()-pat_len:
-                    break
-                print("Found ARM call %08X" % (int(adr[0])))
-                match += 1
-                adr = adr[0] + 1
-                
-            if match == 0:
-                print("data not found [%s]" % bin_str)  
-            else: 
-                print("data not found %d times" % match)  
-            
+        idaapi.msg(f"ENDIAN: {'BE' if is_be else 'LE'}\n")
+
+        # Search normal pattern
+        bin_str = self._generate_pattern(addr, is_be)
+        self._search_pattern(bin_str, is_be)
+
+        # If processor is ARM, adjust address and search again
+        if idc.get_processor_name().startswith("ARM"):
+            idaapi.msg("ARM detected, searching adjusted address...\n")
+            addr += 1
+            bin_str = self._generate_pattern(addr, is_be)
+            self._search_pattern(bin_str, is_be)
 
     def term(self):
         pass
-
 
 def PLUGIN_ENTRY():
     return myplugin_t()
